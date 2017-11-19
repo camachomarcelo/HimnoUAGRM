1f1d8ad2e90e6e33cc0b4eabd61254810aa19012
Marcelo Camacho <www.marcelocamacho@gmail.com>
2017-11-19 11:55:10 -0400

    Definida orientación a sólo Portrait

--- app/src/main/java/com/dranser/himnouagrm/MainActivity.java
+++ app/src/main/java/com/dranser/himnouagrm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dranser.himnouagrm;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
